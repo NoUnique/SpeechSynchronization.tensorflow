6429d7691be7a624998e1779c58ba9f2d58fdf7c
NoUnique <kofmap@gmail.com>
2020-08-04 08:18:56 +0900

    Add serialization script for LRS3 dataset using BeamBasedBuilder

---
+++ lib/datasets/build_lrs3_beam.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# shell script to serialize lrs3 dataset.
+#
+# Author : NoUnique (kofmap@gmail.com)
+# Copyright 2020 NoUnique. All Rights Reserved
+
+TRAINSET_PART_FILES=(
+    "lrs3_pretrain_partaa"
+    "lrs3_pretrain_partab"
+    "lrs3_pretrain_partac"
+    "lrs3_pretrain_partad"
+    "lrs3_pretrain_partae"
+    "lrs3_pretrain_partaf"
+    "lrs3_pretrain_partag"
+)
+TRAINSET_FILE="lrs3_pretrain.zip"
+VALIDSET_FILE="lrs3_trainval.zip"
+TESTSET_FILE="lrs3_test_v0.4.zip"
+
+SCRIPT_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+SRC_DIR=${1:-"/data/lrs3"}
+DST_DIR=${2:-${SRC_DIR}}
+
+set -x  # echo on
+
+cd ${SRC_DIR}
+
+# Check if train(pretrain) set exists
+# If there is no concatenated train set zip file, concatenate it using part files
+if [[ ! -f ${TRAINSET_FILE} ]]; then
+    echo "${TRAINSET_FILE} does not exist. Try to concatenate part files."
+    for file in ${TRAINSET_PART_FILES[@]}; do
+        if [[ ! -f ${file} ]]; then
+          echo "${file} does not exist."
+          exit
+        fi
+    done
+    echo "All part files are exist. Start to concatenate part files."
+    cat ${TRAINSET_PART_FILES[@]} > ${TRAINSET_FILE}
+    echo "${TRAINSET_FILE} is generated"
+fi
+
+# Check if valid(trainval) set exists
+if [[ ! -f ${VALIDSET_FILE} ]]; then
+    echo "${VALIDSET_FILE} does not exist."
+fi
+
+# Check if test set exists
+if [[ ! -f ${TESTSET_FILE} ]]; then
+    echo "${TESTSET_FILE} does not exist."
+fi
+
+cd ${SCRIPT_DIR}
+python -m tensorflow_datasets.scripts.download_and_prepare \
+    --builder_config_id=0 \
+    --max_examples_per_split=10 \
+    --datasets=lrs3 \
+    --module_import=lrs3_beam \
+    --manual_dir=${SRC_DIR} \
+    --data_dir=${DST_DIR} \
+    --beam_pipeline_options="direct_num_workers=0,direct_running_mode=multi_processing"
+    #--debug=True \
+    #--debug_start=True \
+    #--max_examples_per_split=10 \
+    #--beam_pipeline_options="direct_num_workers=0"
+    #--beam_pipeline_options="direct_num_workers=0,direct_running_mode=multi_threading"
+    #--beam_pipeline_options="direct_num_workers=0,direct_running_mode=multi_processing"
---
+++ lib/datasets/lrs3_beam.py
@@ -0,0 +1,243 @@
+# -*- coding: utf-8 -*-
+"""
+    LRS3 dataset from http://www.robots.ox.ac.uk/~vgg/data/lip_reading/lrs3.html
+
+    Author : NoUnique (kofmap@gmail.com)
+    Copyright 2020 NoUnique. All Rights Reserved
+"""
+
+import os
+import random
+import zipfile
+
+import numpy as np
+import tensorflow.compat.v2 as tf
+import tensorflow_datasets.public_api as tfds
+
+_CITATION = """\
+@InProceedings{Afouras18d,
+  author       = "Afouras, T. and Chung, J.~S. and Zisserman, A.",
+  title        = "LRS3-TED: a large-scale dataset for visual speech recognition",
+  booktitle    = "arXiv preprint arXiv:1809.00496",
+  year         = "2018",
+}
+"""
+
+_DESCRIPTION = """\
+LRS3 is an audio-visual speech recognition dataset collected from in the wild videos. \
+The dataset consists of thousands of spoken sentences from TED and TEDx videos. \
+There is no overlap between the videos used to create the test set and \
+the ones used for the pre-train and trainval sets. \
+The dataset statistics are given in the table below.
+"""
+
+
+class Lrs3Config(tfds.core.BuilderConfig):
+    def __init__(self, *, width=None, height=None, sample_rate=16000, **kwargs):
+        """The parameters specifying how the dataset will be processed.
+        If `width` and `height` are set, the videos
+        will be rescaled to have those heights and widths (using ffmpeg).
+        Args:
+          width: An integer with the width or None.
+          height: An integer with the height or None.
+          **kwargs: Passed on to the constructor of `BuilderConfig`.
+        """
+        super(Lrs3Config, self).__init__(**kwargs)
+        if (width is None) ^ (height is None):
+            raise ValueError('Either both dimensions should be set, or none of them')
+        self.width = width
+        self.height = height
+        self.sample_rate = sample_rate
+
+
+class Lrs3(tfds.core.BeamBasedBuilder):
+    """Lip Reading Sentences 3 (LRS3) Dataset
+
+    A audio-visual speech recognition dataset
+    collected from in the wild videos.
+    """
+
+    VERSION = tfds.core.Version('1.0.0')
+
+    MANUAL_DOWNLOAD_INSTRUCTIONS = """\
+    manual_dir should contain two files: vggface2_test.tar.gz and
+    vggface2_train.tar.gz.
+    You need to fill a form(https://goo.gl/forms/vGZmhJaZ9LAklozz2)
+    in order to get the ID and password to download the dataset
+    from http://www.robots.ox.ac.uk/~vgg/data/lip_reading/lrs3.html.
+    """
+
+    BUILDER_CONFIGS = [
+        Lrs3Config(
+            name='Lrs3_256_16000',  # LRS3 dataset originally has 256x256 size.
+            sample_rate=16000,
+            description='256x256 LRS3 dataset with audio data.',
+            version=VERSION,
+        ),
+        Lrs3Config(
+            name='Lrs3_128_16000',
+            description='128x128 LRS3 dataset with audio data.',
+            width=128,
+            height=128,
+            sample_rate=16000,
+            version=VERSION,
+        ),
+    ]
+
+    def _info(self):
+        if self.builder_config.width is not None:
+            if self.builder_config.height is None:
+                raise ValueError('Provide either both height and width or none.')
+            ffmpeg_extra_args = (
+                '-vf', 'scale={}x{}'.format(self.builder_config.height,
+                                            self.builder_config.width))
+        else:
+            ffmpeg_extra_args = []
+
+        video_shape = (
+            None, self.builder_config.height, self.builder_config.width, 3)
+        features = tfds.features.FeaturesDict({
+            'video': tfds.features.Video(video_shape,
+                                         ffmpeg_extra_args=ffmpeg_extra_args,
+                                         encoding_format='jpeg'),
+            'audio': tfds.features.Audio(sample_rate=self.builder_config.sample_rate,
+                                         file_format='pcm',
+                                         dtype=tf.int16),
+            'sentence': tfds.features.Text(),
+            'video_id': tfds.features.Text(),  # YouTube video ID consists of 11 characters
+            'file_name': tfds.features.Text(),
+        })
+        return tfds.core.DatasetInfo(
+            builder=self,
+            description=_DESCRIPTION,
+            features=features,
+            homepage='http://www.robots.ox.ac.uk/~vgg/data/lip_reading/lrs3.html',
+            supervised_keys=('video', 'sentence'),  # supervised_keys is for lip reading only
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager):
+        """Returns SplitGenerators."""
+        train_path = os.path.join(dl_manager.manual_dir, 'lrs3_pretrain.zip')
+        valid_path = os.path.join(dl_manager.manual_dir, 'lrs3_trainval.zip')
+        test_path = os.path.join(dl_manager.manual_dir, 'lrs3_test_v0.4.zip')
+        if not tf.io.gfile.exists(train_path) \
+                or not tf.io.gfile.exists(valid_path) \
+                or not tf.io.gfile.exists(test_path):
+            raise AssertionError(
+                'LRS3 requires manual download of the data. Please download '
+                'the pretrain, trainval and test set and place them into: '
+                '{}, {}, {}'.format(train_path, valid_path, test_path))
+        return [
+            tfds.core.SplitGenerator(
+               name=tfds.Split.TRAIN,
+               gen_kwargs={
+                   'archive_path': train_path,
+               }
+            ),
+            tfds.core.SplitGenerator(
+               name=tfds.Split.VALIDATION,
+               gen_kwargs={
+                   'archive_path': valid_path,
+               }
+            ),
+            tfds.core.SplitGenerator(
+                name=tfds.Split.TEST,
+                gen_kwargs={
+                    'archive_path': test_path,
+                }
+            ),
+        ]
+
+    def _build_pcollection(self, pipeline, archive_path):
+        """Generate examples as dicts."""
+        beam = tfds.core.lazy_imports.apache_beam
+
+        # this functions is adopted from
+        # https://github.com/tensorflow/datasets/blob/master/tensorflow_datasets/core/download/extractor.py
+        def _normpath(path):
+            path = os.path.normpath(path)
+            if (path.startswith('.')
+                    or os.path.isabs(path)
+                    or path.endswith('~')
+                    or os.path.basename(path).startswith('.')):
+                return None
+            return path.replace('\\', '/')  # For windows compatibility
+
+        def _read_audio(fobj):
+            pydub = tfds.core.lazy_imports.pydub
+            audio_segment = pydub.AudioSegment.from_file(fobj, format='mp4')
+            audio_array = np.array(audio_segment.get_array_of_samples())
+            audio = audio_array.astype(np.int16)  # pydub decodes 'mp4' to int16(s16le)
+            return audio
+
+        def _extract_data(inputs):
+            """Extracts files from the zip archives."""
+            # yields examples without extract compressed archive files
+            archive_path = inputs
+            with tf.io.gfile.GFile(archive_path, 'rb') as fobj:
+                z = zipfile.ZipFile(fobj)
+                for member in z.infolist():
+                    # Filter directories and text files # pytype: disable=attribute-error
+                    if member.is_dir() or \
+                            os.path.splitext(member.filename)[-1] != '.mp4':
+                        continue
+                    video_path = _normpath(member.filename)
+                    if not video_path:
+                        continue
+                    video_info = member
+                    yield [archive_path, video_info]
+
+        def _process_example(inputs):
+            archive_path, video_info = inputs
+            with tf.io.gfile.GFile(archive_path, 'rb') as fobj:
+                z = zipfile.ZipFile(fobj)
+
+                video_path = _normpath(video_info.filename)
+                video_fobj = z.open(video_info)
+                video_bytes = video_fobj.read()
+
+                # Caution: Video feature and audio feature should have separated
+                #          file object. Since tfds.features.Video only can receive
+                #          byte-stream. (which is a form of converted file object
+                #          by 'read()') but tfds.features.Audio(using pydub) cannot.
+                audio_fobj = z.open(video_info)
+                audio_pcm = _read_audio(audio_fobj)
+
+                text_path = os.path.splitext(video_path)[0] + '.txt'
+                text_info = z.getinfo(text_path)
+                text_fobj = z.open(text_info)
+                text_bytes = text_fobj.readline()  # first line contains a sentence
+                text_str = text_bytes.decode('utf-8')[len('Text:  '):-len('\n')]
+
+                split, video_id, fname = video_path.split('/')
+                record_id = '_'.join([video_id, os.path.splitext(fname)[0]])
+
+                record = {
+                    'file_name': video_path,
+                    'video': video_bytes,
+                    'audio': audio_pcm,
+                    'sentence': text_str,
+                    'video_id': video_id,
+                }
+                return record_id, record
+
+        # Beam might fuse together the _extract_data and _process_example which
+        # defeats the purpose of parallel processing. As a result, we reshard by
+        # doing a GroupByKey on random keys, and then flattening again.
+        def _add_random_keys(inputs):
+            key = str(random.randrange(10 ** 10))
+            return key, inputs
+
+        def _remove_keys(inputs):
+            _, rows = inputs
+            for row in rows:
+                yield row
+
+        return (pipeline
+                | beam.Create([archive_path])
+                | beam.FlatMap(_extract_data)
+                | beam.Map(_add_random_keys)
+                | beam.GroupByKey()
+                | beam.FlatMap(_remove_keys)
+                | beam.Map(_process_example))
